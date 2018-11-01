ed3eea9d0b8b699db4aa63c43a4d77b5966b3818
mrahtapot <mrahtapot@172.16.110.19>
2018-11-02 00:37:36 +0300

    Commit&Push Test: Square class added

---
+++ src/board/Square.java
@@ -0,0 +1,24 @@
+package board;
+
+public abstract class Square {
+	private String name;
+	private int number;
+	
+	public Square(String name, int number) {
+		super();
+		this.name = name;
+		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+	
+	public void executeAction() {
+		
+	}
+}
