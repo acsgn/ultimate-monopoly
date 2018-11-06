f0a4b72d4025f122db5e426cdeab5aae4f4fa313
acsgn <acosgun16@ku.edu.tr>
2018-11-06 20:32:21 +0300

    Network Done completely, server handles play order for now

--- src/network/Server.java
+++ src/network/Server.java
@@ -3,45 +3,89 @@ package network;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Comparator;
 
-public class Server implements Runnable{
+public class Server implements Runnable {
 
 	private static final int DEFAULT_PORT = 302;
-	
-	private MessageSocket[] players;
 
-	public Server(int numOfPlayers){
-		players = new MessageSocket[numOfPlayers];
+	private ArrayList<MessageSocket> players;
+
+	public Server(int numOfPlayers) {
+		players = new ArrayList<MessageSocket>(numOfPlayers);
 	}
 
 	@Override
 	public void run() {
 		connectPlayers();
-		// Here will go the protocol for messages
+		orderPlayers();
+		play();
+	}
+
+	private void play() {
+		int currentPlayer = 0;
+		while (true) {
+			String message = receiveMessageFromPlayer(currentPlayer);
+			if (message.compareTo("CLOSE") == 0) {
+				sendMessageToOtherPlayers(message, currentPlayer);
+				players.remove(currentPlayer);
+				continue;
+			}
+			sendMessageToOtherPlayers(message, currentPlayer);
+		}
+	}
+
+	private void orderPlayers() {
+		String getDice = "SENDDICE";
+		for (int i = 0; i < players.size(); i++) {
+			sendMessageToPlayer(getDice, i);
+			int diceValue = Integer.parseInt(receiveMessageFromPlayer(i));
+			players.get(i).diceValue = diceValue;
+		}
+		players.sort(new Comparator<MessageSocket>() {
+			@Override
+			public int compare(MessageSocket mS1, MessageSocket mS2) {
+				return Integer.compare(mS1.diceValue, mS2.diceValue);
+			}
+		});
 	}
 
 	private void connectPlayers() {
 		ServerSocket server;
 		try {
 			server = new ServerSocket(DEFAULT_PORT);
-			for (int i = 0; i < players.length; i++) {
+			for (int i = 0; i < players.size(); i++) {
 				Socket s = server.accept();
-				players[i] = new MessageSocket(s);
+				MessageSocket mS = new MessageSocket(s);
+				players.add(mS);
 			}
 			server.close();
 		} catch (IOException e) {
 			System.err.println("Server setup error");
-		}		
-	}
-
-	private void sendMessageToOthers(String message) {
-		for(MessageSocket mS: players) {
-			mS.sendMessage(message);
 		}
 	}
 
-	private String receiveMessageFromOtherPlayer(int turnNumber) throws IOException {
-		return  players[turnNumber].receiveMessage();
+	private void sendMessageToOtherPlayers(String message, int index) {
+		for (int i = 0; i < players.size(); i++) {
+			if (i == index)
+				continue;
+			sendMessageToPlayer(message, index);
+		}
+	}
+
+	private void sendMessageToPlayer(String message, int index) {
+		players.get(index).sendMessage(message);
+	}
+
+	private String receiveMessageFromPlayer(int index) {
+		String message = "";
+		try {
+			message = players.get(index).receiveMessage();
+		} catch (IOException e) {
+			System.err.println("Message Receive Error");
+		}
+		return message;
 	}
 
 }
