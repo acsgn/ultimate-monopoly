c36e123d4631a832f63e67162b0269f983bb5099
amasry <amasry17@ku.edu.tr>
2018-11-09 11:58:36 -0800

    Started Constructing the squares

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,19 +1,36 @@
 package game;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import game.square.GoSquare;
 import game.square.Square;
 
 public class Board {
 	private List<Square> squares; 
+	private static Board board; 
 	
-	public Board(){
+	private Board(){
+		squares = new ArrayList<>();
 		constructSquares();
 	}
+	
+	public static Board getInstance(){
+		if(board==null){
+			board = new Board();
+		}
+		return board;
+	}
 	public void constructSquares(){
+		GoSquare go = new GoSquare("GO",1);
+		squares.add(go);
 		
 	}
 	public Square getSquare(int index){
 		return squares.get(index);
 	}
+	public int getNoOfSquares(){
+		return squares.size();
+	}
+		
 }
