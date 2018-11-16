d2b2799b3baf0e007dbe06187adfd0739171c185
amasry <amasry17@ku.edu.tr>
2018-11-16 02:15:31 -0800

    Initializing the cards arrayLists

--- src/game/ActionCards.java
+++ src/game/ActionCards.java
@@ -13,7 +13,36 @@ public class ActionCards {
 	private ArrayList<CommunityChest> communityChest;
 	private ArrayList<RollThree> rollThree;
 
+	private static ActionCards actionCards;
 	
+	private ActionCards(){
+		constructCommunityChestCards();
+		constructChanceCards();
+	}
+	public static ActionCards getInstance(){
+		if(actionCards==null){
+			actionCards = new ActionCards();
+		}
+		return actionCards;
+	}
+	
+	private void constructCommunityChestCards(){
+		communityChest = new ArrayList<>();
+		CommunityChest incomeTaxrefundCard = new CommunityChest("Income Tax Refund");
+		communityChest.add(incomeTaxrefundCard);
+	}
+	private void constructChanceCards(){
+		chance = new ArrayList<>();
+		Chance partyTime = new Chance("PARTY TIME");
+		chance.add(partyTime);
+	}
+	public CommunityChest getCommunityChestCard(){
+		// should return a card randomly
+		return communityChest.get(communityChest.size()-1);
+	}
+	public Chance getChanceCard(){
+		return chance.get(chance.size()-1);
+	}
 	public void putTheCardBack(Action card) {
 
 	}
