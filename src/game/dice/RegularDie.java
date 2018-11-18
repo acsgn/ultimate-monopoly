3ec037f81ade5fdf64d370b5b8bf21f722a06842
acsgn <acosgun16@ku.edu.tr>
2018-11-18 20:15:27 +0300

    Dice is random

--- src/game/dice/RegularDie.java
+++ src/game/dice/RegularDie.java
@@ -6,19 +6,16 @@ public class RegularDie extends Dice {
 	public static final int NORMAL_MAX = 6;
 
 	public RegularDie() {
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void roll() {
-		// TODO Auto-generated method stub
-		Random rand = new Random(10);
+		Random rand = new Random(System.nanoTime());
 		faceValue = rand.nextInt(NORMAL_MAX)+1;
 	}
 
 	@Override
 	public int getFaceValue() {
-		// TODO Auto-generated method stub
 		return faceValue;
 	}
 
