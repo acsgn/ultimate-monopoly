917edacafdaf1dd952f1d5f8d869270a651a9a3a
Ayca Kaygusuz <akaygusuz14@ku.edu.tr>
2018-12-06 00:34:44 +0300

    Added adjustments with the new BuildingType class to the related classes.

--- src/game/building/Building.java
+++ src/game/building/Building.java
@@ -1,8 +1,16 @@
 package game.building; 
 
 public abstract class Building {
-
+	
+	private BuildingType buildingType;
+	
+	public BuildingType getBuildingType() {
+		return buildingType;
+	}
+	
 	public Building() {
+		
+		
 	}
 	
 }
--- src/game/building/House.java
+++ src/game/building/House.java
@@ -1,6 +1,12 @@
 package game.building; 
 public class House extends Building {
 
+	private static final BuildingType type = BuildingType.HOUSE;
+	
+	public BuildingType getBuildingType() {
+		return type;
+	}
+	
 	public House() {
 		super();
 		// TODO Auto-generated constructor stub
