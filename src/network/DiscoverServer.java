ba981ac6eac4c22c8565ec887ebc3dbce4ee992a
acosgun16 <acosgun16@ku.edu.tr>
2018-12-07 12:43:50 +0300

    Discovery reset

--- src/network/DiscoverServer.java
+++ src/network/DiscoverServer.java
@@ -10,17 +10,22 @@ public class DiscoverServer {
 	public static void main(String[] args) {
 		DatagramSocket socket;
 		try {
-			socket = new DatagramSocket(302, InetAddress.getByName("0.0.0.0"));
+			socket = new DatagramSocket(8888, InetAddress.getByName("0.0.0.0"));
 			socket.setBroadcast(true);
 			while (true) {
-				byte[] recvBuf = new byte[2048];
+				System.out.println(">>>Ready to receive broadcast packets!");
+
+				// Receive a packet
+				byte[] recvBuf = new byte[15000];
 				DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
 				socket.receive(packet);
 				System.out.println(">>>Discovery packet received from: " + packet.getAddress().getHostAddress());
 				System.out.println(">>>Packet received; data: " + new String(packet.getData()));
+				// See if the packet holds the right command (message)
 				String message = new String(packet.getData()).trim();
-				if (message.equals("ULTIMATE_MONOPOLY_REQUEST")) {
-					byte[] sendData = "ULTIMATE_MONOPOLY_RESPONSE".getBytes();
+				if (message.equals("DISCOVER_FUIFSERVER_REQUEST")) {
+					byte[] sendData = "DISCOVER_FUIFSERVER_RESPONSE".getBytes();
+					// Send a response
 					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, packet.getAddress(),
 							packet.getPort());
 					socket.send(sendPacket);
@@ -29,6 +34,5 @@ public class DiscoverServer {
 			}
 		} catch (IOException ex) {
 		}
-
 	}
 }
--- src/network/DiscoveryClient.java
+++ src/network/DiscoveryClient.java
@@ -15,41 +15,96 @@ public class DiscoveryClient {
 		DatagramSocket c;
 		try {
 			c = new DatagramSocket();
+
 			c.setBroadcast(true);
-			byte[] sendData = "ULTIMATE_MONOPOLY_REQUEST".getBytes();
+
+			byte[] sendData = "DISCOVER_FUIFSERVER_REQUEST".getBytes();
+
 			try {
+
 				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,
-						InetAddress.getByName("255.255.255.255"), 302);
+						InetAddress.getByName("255.255.255.255"), 8888);
+
 				c.send(sendPacket);
-				System.out.println(">>> Request packet sent to: 255.255.255.255 (DEFAULT)");
+
+				System.out.println( ">>> Request packet sent to: 255.255.255.255 (DEFAULT)");
+
 			} catch (Exception e) {
 			}
+
+			// Broadcast the message over all the network interfaces
+
 			Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+
 			while (interfaces.hasMoreElements()) {
+
 				NetworkInterface networkInterface = interfaces.nextElement();
-				if (networkInterface.isLoopback() || !networkInterface.isUp())
-					continue;
-				for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
-					InetAddress broadcast = interfaceAddress.getBroadcast();
-					if (broadcast == null)
-						continue;
-					try {
-						DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, broadcast, 302);
-						c.send(sendPacket);
-					} catch (Exception e) {
-					}
-					System.out.println(">>> Request packet sent to: " + broadcast.getHostAddress() + "; Interface: "
-							+ networkInterface.getDisplayName());
+
+				if (networkInterface.isLoopback() || !networkInterface.isUp()) {
+
+					continue; // Don't want to broadcast to the loopback interface
+
 				}
+
+				for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
+
+					InetAddress broadcast = interfaceAddress.getBroadcast();
+
+					if (broadcast == null) {
+
+						continue;
+
+					}
+
+					// Send the broadcast package!
+
+					try {
+
+						DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, broadcast, 8888);
+
+						c.send(sendPacket);
+
+					} catch (Exception e) {
+
+					}
+
+					System.out.println( ">>> Request packet sent to: "
+							+ broadcast.getHostAddress() + "; Interface: " + networkInterface.getDisplayName());
+
+				}
+
 			}
-			System.out.println(">>> Done looping over all network interfaces. Now waiting for a reply!");
-			byte[] recvBuf = new byte[2048];
+
+			System.out.println( ">>> Done looping over all network interfaces. Now waiting for a reply!");
+
+			// Wait for a response
+
+			byte[] recvBuf = new byte[15000];
+
 			DatagramPacket receivePacket = new DatagramPacket(recvBuf, recvBuf.length);
+
 			c.receive(receivePacket);
+
+			// We have a response
+
+			System.out.println( ">>> Broadcast response from server: "
+					+ receivePacket.getAddress().getHostAddress());
+
+			// Check if the message is correct
+
 			String message = new String(receivePacket.getData()).trim();
-			if (message.equals("ULTIMATE_MONOPOLY_RESPONSE"))
+
+			if (message.equals("DISCOVER_FUIFSERVER_RESPONSE")) {
+
+				// DO SOMETHING WITH THE SERVER'S IP (for example, store it in your controller)
+
 				System.out.println(receivePacket.getAddress());
+
+			}
+			// Close the port!
+
 			c.close();
+
 		} catch (IOException ex) {
 		}
 	}
