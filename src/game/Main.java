834912466f62a4edcc6459987d4dc9aaf4286fcc
acosgun16 <acosgun16@ku.edu.tr>
2018-12-09 17:03:10 +0300

    Little update

--- src/game/Main.java
+++ src/game/Main.java
@@ -9,12 +9,12 @@ public class Main {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		MonopolyGame game = new MonopolyGame();
-		Controller.getInstance().initialize(game);
 		UIScreen screen = new UIScreen();
 		Controller.getInstance().addGamelistener(screen);
 		UICreator gameCreator = new UICreator();
 		Controller.getInstance().addGamelistener(gameCreator);
+		MonopolyGame game = new MonopolyGame();
+		Controller.getInstance().setGame(game);
 		gameCreator.setVisible(true);
 	}
 
